b5a4fd624aedab6f358f4efe71b41f4b71732060
brandonwernham <brandonwern01@gmail.com>
2023-03-31 11:20:34 -0400

    Procedural nature and wall generation

--- Toyland-Takedown/Assets/Scripts/ProceduralGeneration.cs
+++ Toyland-Takedown/Assets/Scripts/ProceduralGeneration.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class ProceduralGeneration : MonoBehaviour
 {
     public GameObject[] tilePrefabs;
+    public GameObject wallPrefab;
     public int mapRows;
     public int mapColumns;
     public float tileSize;
+    public float wallHeight = 1f;
+    public bool generateBorderWall = true;
 
     private void Start()
     {
         GenerateMap();
+
+        if (generateBorderWall)
+        {
+            GenerateBorderWall();
+        }
     }
 
     private void GenerateMap()
@@ -24,6 +32,29 @@ public class ProceduralGeneration : MonoBehaviour
                 Vector3 position = new Vector3(x * tileSize, 0, z * tileSize);
                 GameObject tileInstance = Instantiate(tilePrefab, position, Quaternion.Euler(0, 90 * Random.Range(0, 4), 0));
                 tileInstance.transform.parent = transform;
+            }
+        }
+    }
+
+    private void GenerateBorderWall()
+    {
+        float wallSize = 4.0f;
+        int wallUnitsPerTile = Mathf.CeilToInt(tileSize / wallSize);
+
+        for (int x = -wallUnitsPerTile; x < (mapRows * wallUnitsPerTile) - wallUnitsPerTile + 1; x++)
+        {
+            for (int z = -wallUnitsPerTile; z < (mapColumns * wallUnitsPerTile) - wallUnitsPerTile + 1; z++)
+            {
+                if (x == -wallUnitsPerTile || z == -wallUnitsPerTile || x == (mapRows * wallUnitsPerTile) - wallUnitsPerTile || z == (mapColumns * wallUnitsPerTile) - wallUnitsPerTile)
+                {
+                    for (int y = 0; y < wallHeight; y++)
+                    {
+                        Vector3 position = new Vector3((x * wallSize) + (tileSize / 2), (y * wallSize) + (wallSize / 2), (z * wallSize) + (tileSize / 2));
+                        Quaternion rotation = Quaternion.identity;
+                        GameObject wallInstance = Instantiate(wallPrefab, position, rotation);
+                        wallInstance.transform.parent = transform;
+                    }
+                }
             }
         }
     }
